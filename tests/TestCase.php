e5a551b17f0a8624706aaeb9aa9046f0dd059681
AL!R3Z4 <alireza.jazayerei@gmail.com>
2021-09-30 20:46:16 +0330

    set up config and db

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,10 +3,17 @@
 namespace Alirzaj\ElasticsearchBuilder\Tests;
 
 use Alirzaj\ElasticsearchBuilder\ElasticsearchBuilderServiceProvider;
+use Alirzaj\ElasticsearchBuilder\Tests\Indices\Blogs;
+use Alirzaj\ElasticsearchBuilder\Tests\Indices\Users;
+use Alirzaj\ElasticsearchBuilder\Tests\Models\Blog;
+use Alirzaj\ElasticsearchBuilder\Tests\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Orchestra\Testbench\TestCase as Orchestra;
 
 class TestCase extends Orchestra
 {
+    use RefreshDatabase;
+
     protected function setUp(): void
     {
         parent::setUp();
@@ -19,7 +26,32 @@ class TestCase extends Orchestra
         ];
     }
 
-    public function getEnvironmentSetUp($app)
+    /**
+     * Define environment setup.
+     *
+     * @param \Illuminate\Foundation\Application $app
+     * @return void
+     */
+    protected function defineEnvironment($app)
     {
+        $app['config']->set('database.connections.mysql.database', 'elastic-builder');
+        $app['config']->set('database.connections.mysql.username', 'root');
+        $app['config']->set('database.connections.mysql.password', '');
+
+        $app['config']->set('elasticsearch.indices', [
+            Blog::class => Blogs::class,
+            User::class => Users::class
+        ]);
+    }
+
+    /**
+     * Define database migrations.
+     *
+     * @return void
+     */
+    protected function defineDatabaseMigrations()
+    {
+        $this->loadLaravelMigrations();
+        $this->loadMigrationsFrom(__DIR__ . DIRECTORY_SEPARATOR . 'Migrations');
     }
 }
