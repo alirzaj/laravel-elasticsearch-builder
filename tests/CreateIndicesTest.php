1d099ae933d0b696d99f91cc64429fa521cd64fb
AL!R3Z4 <alireza.jazayerei@gmail.com>
2021-09-30 20:46:32 +0330

    add more assertion

--- tests/CreateIndicesTest.php
+++ tests/CreateIndicesTest.php
@@ -1,68 +1,98 @@
 <?php
 
-use Alirzaj\ElasticsearchBuilder\Tests\Indices\Users;
 use Elasticsearch\Client;
 use Elasticsearch\Namespaces\IndicesNamespace;
 use function Pest\Laravel\artisan;
 
 it('can create indices', function () {
-    config()->set('elasticsearch.indices', ['user' => Users::class]);
-
     $client = \Pest\Laravel\mock(Client::class);
     $indices = \Pest\Laravel\mock(IndicesNamespace::class);
 
     $client
         ->shouldReceive('indices')
-        ->once()
+        ->twice()
         ->andReturn($indices);
 
     $indices
         ->shouldReceive('create')
         ->once()
+        ->with(
+            [
+                'index' => 'users_index',
+                'body' => [
+                    'settings' => [
+                        'analysis' => [
+                            'analyzer' => [
+                                'hashtag' => [
+                                    'type' => 'custom',
+                                    'tokenizer' => 'hashtag_tokenizer',
+                                    'filter' => ['lowercase']
+                                ]
+                            ],
+                            'tokenizer' => [
+                                'hashtag_tokenizer' => [
+                                    'type' => 'pattern',
+                                    'pattern' => '#\S+',
+                                    'group' => 0
+                                ]
+                            ]
+                        ]
+                    ],
+                    'mappings' => [
+                        'properties' => [
+                            'text' => [
+                                'type' => 'text',
+                                'fields' => [
+                                    'hashtags' => [
+                                        'type' => 'text',
+                                        'analyzer' => 'hashtag'
+                                    ]
+                                ]
+                            ],
+                            'user_id' => [
+                                'type' => 'keyword',
+                                'fields' => []
+                            ],
+                            'ip' => [
+                                'type' => 'ip',
+                                'fields' => []
+                            ],
+                            'created_at' => [
+                                'type' => 'date',
+                                'fields' => []
+                            ],
+                        ],
+                    ]
+                ]
+            ],
+        );
+
+    $indices
+        ->shouldReceive('create')
+        ->once()
         ->with([
-            'index' => 'users_index',
+            'index' => 'blogs',
             'body' => [
                 'settings' => [
                     'analysis' => [
-                        'analyzer' => [
-                            'hashtag' => [
-                                'type' => 'custom',
-                                'tokenizer' => 'hashtag_tokenizer',
-                                'filter' => ['lowercase']
-                            ]
-                        ],
-                        'tokenizer' => [
-                            'hashtag_tokenizer' => [
-                                'type' => 'pattern',
-                                'pattern' => '#\S+',
-                                'group' => 0
-                            ]
-                        ]
+                        'analyzer' => [],
+                        'tokenizer' => []
                     ]
                 ],
                 'mappings' => [
                     'properties' => [
                         'text' => [
                             'type' => 'text',
-                            'fields' => [
-                                'hashtags' => [
-                                    'type' => 'text',
-                                    'analyzer' => 'hashtag'
-                                ]
-                            ]
+                            'fields' => []
                         ],
-                        'user_id' => [
+                        'title' => [
                             'type' => 'keyword',
                             'fields' => []
                         ],
-                        'ip' => [
-                            'type' => 'ip',
+                        'description' => [
+                            'type' => 'completion',
                             'fields' => []
-                        ],
-                        'created_at' => [
-                            'type' => 'date',
-                            'fields' => []
-                        ],
+                        ]
                     ],
                 ]
             ]
