1fde3965b15287fff23965d53c56f76f9d03908d
AL!R3Z4 <alireza.jazayerei@gmail.com>
2021-09-30 20:45:47 +0330

    add queue option & comment for each key

--- config/elasticsearch.php
+++ config/elasticsearch.php
@@ -1,8 +1,20 @@
 <?php
 
 return [
+    /**
+     * the hosts of elasticsearch.
+     * this can be a string or an array of hosts
+     */
     'hosts' => 'localhost:9200',
 
+    /**
+     * the name of the queue for all jobs related to elasticsearch
+     */
+    'queue' => 'elasticsearch',
+
+    /**
+     * this array connects models to their index class
+     */
     'indices' => [
         // 'Model::class' => Index::Class
     ]
