396d82c9d13d700ea89137ff62e5fcf49921287f
AL!R3Z4 <alireza.jazayerei@gmail.com>
2021-10-31 21:09:57 +0330

    add/remove array items

---
+++ src/Jobs/RemoveArrayItem.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Alirzaj\ElasticsearchBuilder\Jobs;
+
+use Elasticsearch\Client;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+class RemoveArrayItem implements ShouldQueue
+{
+    use Dispatchable;
+    use InteractsWithQueue;
+    use Queueable;
+    use SerializesModels;
+
+    public function __construct(
+        private string $name,
+        private string $field,
+        private mixed  $search
+    )
+    {
+        $this->onQueue(config('elasticsearch.queue'));
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle(Client $client)
+    {
+        $client->updateByQuery([
+            'refresh' => true,
+            'index' => $this->name,
+            'body' => [
+                'script' => [
+                    'source' => "ctx._source.$this->field.remove(ctx._source.$this->field.indexOf(params.search));",
+                    'params' => [
+                        'search' => $this->search,
+                    ]
+                ],
+                'query' => [
+                    'bool' => [
+                        'filter' => [
+                            'term' => [$this->field => $this->search]
+                        ]
+                    ]
+                ]
+            ]
+        ]);
+    }
+}
---
+++ src/Jobs/UpdateArrayItem.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Alirzaj\ElasticsearchBuilder\Jobs;
+
+use Elasticsearch\Client;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+class UpdateArrayItem implements ShouldQueue
+{
+    use Dispatchable;
+    use InteractsWithQueue;
+    use Queueable;
+    use SerializesModels;
+
+    public function __construct(
+        private string $name,
+        private string $field,
+        private mixed  $search,
+        private mixed  $replace
+    )
+    {
+        $this->onQueue(config('elasticsearch.queue'));
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle(Client $client)
+    {
+        $client->updateByQuery([
+            'refresh' => true,
+            'index' => $this->name,
+            'body' => [
+                'script' => [
+                    'source' => "
+                        ctx._source.$this->field.remove(ctx._source.$this->field.indexOf(params.search));
+                        ctx._source.$this->field.add(params.replace);
+                        ",
+                    'params' => [
+                        'search' => $this->search,
+                        'replace' => $this->replace
+                    ]
+                ],
+                'query' => [
+                    'bool' => [
+                        'filter' => [
+                            'term' => [$this->field => $this->search]
+                        ]
+                    ]
+                ]
+            ]
+        ]);
+    }
+}
