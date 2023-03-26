de750ff16ddbe79eb84bfde2835a0f4e7fee846c
alireza <alireza-jaz@bd.ir>
2023-03-26 13:10:06 +0430

    ability to update documents having some conditions

---
+++ src/Jobs/UpdateDocumentsByCondition.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace Alirzaj\ElasticsearchBuilder\Jobs;
+
+use Alirzaj\ElasticsearchBuilder\Query\Filter;
+use Alirzaj\ElasticsearchBuilder\Query\MustNot;
+use Alirzaj\ElasticsearchBuilder\Query\Query;
+use Elasticsearch\Client;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Arr;
+
+class UpdateDocumentsByCondition implements ShouldQueue
+{
+    use Dispatchable;
+    use InteractsWithQueue;
+    use Queueable;
+    use SerializesModels;
+
+
+    public function __construct(
+        private string $name,
+        private array  $conditions,
+        private array  $document,
+        private array  $largeFields = []
+    )
+    {
+        $this->onQueue(config('elasticsearch.queue'));
+    }
+
+    public function handle(Client $client): void
+    {
+        $query = (new Query())
+            ->addIndex($this->name)
+            ->only('')
+            ->size(10000) //elasticsearch MAX_WINDOW_SIZE is 10000
+            ->boolean(
+                ...collect($this->conditions)
+                ->map(fn($value, string $field) => is_null($value)
+                    ? fn(MustNot $mustNot) => $mustNot->exists($field)
+                    : fn(Filter $filter) => $filter->terms($field, Arr::wrap($value))
+                )
+                ->values()
+                ->toArray()
+            );
+
+        /**
+         * script size is limited in elasticsearch.
+         * therefor we must use bulk operation
+         * if we want to update a large field
+         */
+        if (blank($this->largeFields) || !Arr::hasAny($this->document, $this->largeFields)) {
+            $client->updateByQuery([
+                'refresh' => true,
+                'index' => $this->name,
+                'body' => [
+                    'script' => [
+                        'source' => $this->setValueForMultipleFieldsScript($this->document),
+                        'params' => $this->document,
+                    ],
+                    'query' => $query->toRaw()['body']['query'],
+                ],
+            ]);
+
+            return;
+        }
+
+        $ids = $query->get()->keys()->toArray();
+
+        if (blank($ids)) {
+            return;
+        }
+
+        $bulkBody = [];
+        foreach ($ids as $documentId) {
+            $bulkBody[] = ['update' => ['_index' => $this->name, '_id' => $documentId]];
+            $bulkBody[] = ['doc' => $this->document];
+        }
+
+        $client->bulk(['refresh' => true, 'body' => $bulkBody,]);
+    }
+
+    private function setValueForMultipleFieldsScript(array $data): string
+    {
+        $result = '';
+
+        foreach ($data as $field => $value) {
+            $result .= "ctx._source.$field = params.$field; ";
+        }
+
+        return $result;
+    }
+}
---
+++ tests/UpdateDocumentsByConditionTest.php
@@ -0,0 +1,125 @@
+<?php
+
+use Alirzaj\ElasticsearchBuilder\Jobs\UpdateDocumentsByCondition;
+use Elasticsearch\Client;
+
+it('can update documents without large fields in them', function () {
+    \Pest\Laravel\mock(Client::class)
+        ->shouldReceive('updateByQuery')
+        ->with([
+            "refresh" => true,
+            "index" => "blogs",
+            "body" => [
+                "script" => [
+                    "source" => "ctx._source.my-field = params.my-field; ",
+                    "params" => [
+                        "my-field" => "new-value"
+                    ],
+                ],
+                "query" => [
+                    "bool" => [
+                        "filter" => [
+                            [
+                                "terms" => [
+                                    "condition-field" => ["condition-value"]
+                                ]
+                            ]
+                        ],
+                        "must_not" => [
+                            [
+                                "exists" => [
+                                    "field" => "condition-field-2"
+                                ]
+                            ]
+                        ]
+                    ]
+                ]
+            ]
+        ])
+        ->andReturn([]);
+
+    UpdateDocumentsByCondition::dispatchSync(
+        'blogs',
+        [
+            'condition-field' => 'condition-value',
+            'condition-field-2' => null,
+        ],
+        ['my-field' => 'new-value']
+    );
+
+    expect(true)->toBeTrue();
+});
+
+it('can update documents with large fields in them', function () {
+    \Pest\Laravel\mock(Client::class)
+        ->shouldReceive('search')
+        ->with([
+            'index' => ['blogs'],
+            'body' => [
+                '_source' => '',
+                'size' => 10000,
+                "query" => [
+                    "bool" => [
+                        "filter" => [
+                            [
+                                "terms" => [
+                                    "condition-field" => ["condition-value"]
+                                ]
+                            ]
+                        ],
+                        "must_not" => [
+                            [
+                                "exists" => [
+                                    "field" => "condition-field-2"
+                                ]
+                            ]
+                        ]
+                    ]
+                ]
+            ]
+        ])
+        ->andReturn([
+            'hits' => [
+                'hits' => [
+                    ['_source' => [], '_id' => 1],
+                    ['_source' => [], '_id' => 2],
+                    ['_source' => [], '_id' => 3],
+                ],
+            ],
+        ])
+        ->shouldReceive('bulk')
+        ->with(['refresh' => true, 'body' => [
+            ['update' => ['_index' => 'blogs', '_id' => 1]],
+            ['doc' => ['my-field' => 'new-value']],
+            ['update' => ['_index' => 'blogs', '_id' => 2]],
+            ['doc' => ['my-field' => 'new-value']],
+            ['update' => ['_index' => 'blogs', '_id' => 3]],
+            ['doc' => ['my-field' => 'new-value']],
+        ]])
+        ->andReturn([]);
+
+   /* \Pest\Laravel\mock(Client::class)
+        ->shouldReceive('bulk')
+        ->with(['refresh' => true, 'body' => [
+            ['update' => ['_index' => 'blogs', '_id' => 1]],
+            ['doc' => ['my-field' => 'new-value']],
+            ['update' => ['_index' => 'blogs', '_id' => 2]],
+            ['doc' => ['my-field' => 'new-value']],
+            ['update' => ['_index' => 'blogs', '_id' => 3]],
+            ['doc' => ['my-field' => 'new-value']],
+        ]])
+        ->andReturn([]);*/
+
+
+    UpdateDocumentsByCondition::dispatchSync(
+        'blogs',
+        [
+            'condition-field' => 'condition-value',
+            'condition-field-2' => null,
+        ],
+        ['my-field' => 'new-value'],
+        ['my-field']
+    );
+
+    expect(true)->toBeTrue();
+});
