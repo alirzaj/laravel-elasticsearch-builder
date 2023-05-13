6c22b7682b3266362cc31bffa5ec51429e3db663
alireza <alireza-jaz@bd.ir>
2023-05-13 15:25:30 +0330

    add size option

--- src/Query/Aggregation/Terms.php
+++ src/Query/Aggregation/Terms.php
@@ -4,12 +4,18 @@ namespace Alirzaj\ElasticsearchBuilder\Query\Aggregation;
 
 class Terms extends Aggregation
 {
-    public function __construct(protected string $field)
+    public function __construct(protected string $field, public int|null $size = null)
     {
     }
 
     public function toRaw(): array
     {
-       return ['field' => $this->field];
+        $raw = ['field' => $this->field];
+
+        if (!is_null($this->size)) {
+            $raw['size'] = $this->size;
+        }
+
+        return $raw;
     }
 }
